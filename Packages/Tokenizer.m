ef6d9921168987381de6f0ea3e7d717ebfa70540
GalAster <galaster@foxmail.com>
2018-08-30 10:49:12 +0800

    架构::First Blood!

---
+++ Packages/Tokenizer.m
@@ -0,0 +1,76 @@
+(* ::Package:: *)
+(* ::Title:: *)
+(*Tokenizer(Tokenizer)*)
+(* ::Subchapter:: *)
+(*程序包介绍*)
+(* ::Text:: *)
+(*Mathematica Package*)
+(*Created by Mathematica Plugin for IntelliJ IDEA*)
+(*Establish from GalAster's template(v1.3)*)
+(**)
+(* ::Text:: *)
+(*Author:Aster*)
+(*Creation Date:2018-08-30*)
+(*Copyright: Mozilla Public License Version 2.0*)
+(* ::Program:: *)
+(*1.软件产品再发布时包含一份原始许可声明和版权声明。*)
+(*2.提供快速的专利授权。*)
+(*3.不得使用其原始商标。*)
+(*4.如果修改了源代码，包含一份代码修改说明。*)
+(**)
+(* ::Text:: *)
+(*这里应该填这个函数的介绍*)
+(* ::Section:: *)
+(*函数说明*)
+ExampleFunction::usage = "这里应该填这个函数的说明,如果要换行用\"\\r\"\r就像这样";
+(* ::Section:: *)
+(*程序包正体*)
+(* ::Subsection::Closed:: *)
+(*主设置*)
+ExNumber::usage = "程序包的说明,这里抄一遍";
+Begin["`Tokenizer`"];
+(* ::Subsection::Closed:: *)
+(*主体代码*)
+Version$Tokenizer = "V1.0";
+Updated$Tokenizer = "2018-08-30";
+(* ::Subsubsection:: *)
+(*功能块 1*)
+classBasicTokenizer = JavaNew[LoadJavaClass["com.hankcs.hanlp.tokenizer.BasicTokenizer"]];
+BasicTokenizer[str_] := Block[
+	{objs, segment, toString},
+	objs = JavaObjectToExpression@classBasicTokenizer@segment[str];
+	Through[objs@toString[]]
+];
+classStandardTokenizer = JavaNew[LoadJavaClass["com.hankcs.hanlp.tokenizer.StandardTokenizer"]];
+StandardTokenizer[str_] := Block[
+	{objs, segment, toString},
+	objs = JavaObjectToExpression@classStandardTokenizer@segment[str];
+	Through[objs@toString[]]
+];
+classIndexTokenizer = JavaNew[LoadJavaClass["com.hankcs.hanlp.tokenizer.IndexTokenizer"]];
+IndexTokenizer[str_] := Block[
+	{objs, segment, toString},
+	objs = JavaObjectToExpression@classIndexTokenizer@segment[str];
+	Through[objs@toString[]]
+];
+classNLPTokenizer = JavaNew[LoadJavaClass["com.hankcs.hanlp.tokenizer.NLPTokenizer"]];
+NLPTokenizer[str_] := Block[
+	{objs, segment, toString},
+	objs = JavaObjectToExpression@classNLPTokenizer@segment[str];
+	Through[objs@toString[]]
+];
+
+
+
+(* ::Subsubsection:: *)
+(*功能块 2*)
+ExampleFunction[2] = "我就是个示例函数,什么功能都没有";
+
+
+(* ::Subsection::Closed:: *)
+(*附加设置*)
+SetAttributes[
+	{ },
+	{Protected, ReadProtected}
+]
+End[]
---
+++ PacletInfo.m
@@ -0,0 +1,11 @@
+Paclet[
+  Name -> "ChineseNLP",
+  Version -> "1.0.0",
+  Extensions -> {
+    	{
+     		"Kernel",
+     		"Root" -> ".",
+     		"Context" -> {"ChineseNLP`"}
+     	}
+    }
+ ]
