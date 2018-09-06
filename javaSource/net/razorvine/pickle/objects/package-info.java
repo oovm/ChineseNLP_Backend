1dc6c760bd706924890845f04ccc320fc1664878
GalAster <galaster@foxmail.com>
2018-09-06 20:44:07 +0800

    架构::buildin java classes

---
+++ javaSource/net/razorvine/pickle/objects/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Object constructors and other utility classes for the pickle package.
+ * 
+ * @author Irmen de Jong (irmen@razorvine.net)
+ * @version 4.21
+ * @see net.razorvine.pickle
+ */
+package net.razorvine.pickle.objects;
