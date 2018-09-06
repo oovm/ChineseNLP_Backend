e9b070fe626362d44dddab05c56f8f49a6b1788c
GalAster <galaster@foxmail.com>
2018-09-06 20:55:18 +0800

    架构::add vuepress docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,63 @@
+module.exports = {
+	dest: 'docs/.build',
+	locales: {
+		'/': {
+			lang: 'zh-CN',
+			title: 'ChineseNLP',
+			description: 'No game no life ♕'
+		}
+	},
+	head: [
+		['link', {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css'}]
+	],
+	themeConfig: {
+		repo: 'GalAster/Deus',
+		editLinks: true,
+		docsDir: 'docs',
+		markdown: {
+			lineNumbers: true
+		},
+		sidebar: [
+			{
+				title: '开发文档',
+				children: [
+					'/Start/',
+					'/Start/Developer.md',
+					'/Start/Editor.md',
+					'/Start/EditorAdv.md'
+				]
+			},
+			{
+				title: 'MagicSquare 模块',
+				children: [
+					'/MagicSquare/'
+				]
+			},
+			{
+				title: 'NumberMaster 模块',
+				children: [
+					'/NumberMaster/'
+				]
+			},
+			{
+				title: 'HanoiTower 模块',
+				children: [
+					'/HanoiTower/'
+				]
+			},
+			{
+				title: 'Sudoku 模块',
+				children: [
+					'/Sudoku/',
+					'/Sudoku/Object.md'
+				]
+			}
+		]
+	},
+	serviceWorker: true,
+	markdown: {
+		config: md => {
+			md.use(require("markdown-it-katex"));
+		}
+	}
+};
